fd85c66ae51dcc9decd188af58cb5aaff5a7088f
ADITYA VYAS <adityavyas2507@gmail.com>
2026-07-19 18:46:42 +0530

    Add LeetCode Solution #633

---
+++ MEDIUM/633_Sum_Of_square_Numbers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int low = 0;
+        int high = (int)Math.sqrt(c);
+        while(low<=high){
+
+            long ans = (long)low * low + (long)high * high;
+
+            if(ans == c){
+                return true;
+            }
+            else if(ans > c){
+                high--;
+            }
+            else{
+                low++;
+            }
+        }
+        return false;
+    }
+}
